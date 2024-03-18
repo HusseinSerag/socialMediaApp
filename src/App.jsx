bab3ff113aace92e15360acfa172188c8344784c
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2024-03-18 22:23:38 +0200

    Create initial pages login and register

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+const Homepage = lazy(() => import("./pages/homepage"));
+function App() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Suspense>
+  );
+}
+
+export default App;
