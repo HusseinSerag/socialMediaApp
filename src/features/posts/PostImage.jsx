842baa466db81daf17f7d66f4f813a5ab4a705e0
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2024-04-15 17:36:32 +0200

    show posts of on small screens

--- src/features/posts/PostImage.jsx
+++ src/features/posts/PostImage.jsx
@@ -13,50 +13,28 @@ export default function PostImage({ postPhotos }) {
     setCurrentImg((curr) => (curr === 0 ? postPhotos.length - 1 : --curr));
   }
 
-  // useEffect(function () {
-  //   const container = divRef.current;
-
-  //   function onFocus() {}
-  //   container.addEventListener("focus", onFocus);
-
-  //   return () => container.removeEventListener("focus", onFocus);
-  // });
-
   const src = postPhotos[currentImg].photo;
   const id = postPhotos[currentImg].id;
   return (
-    <Modal>
-      <div className="relative flex justify-center overflow-hidden rounded-lg">
-        {postPhotos.length > 1 && (
-          <MdOutlineNavigateBefore
-            onClick={previous}
-            className="absolute left-0 top-[50%]  h-12 w-12 cursor-pointer"
-          />
-        )}
-        <Modal.Toggle
-          opens={`post_id_${id}`}
-          render={(click) => (
-            <div className="h-[500px] max-h-[500px] ">
-              <img
-                onClick={click}
-                src={src}
-                className="hidden h-full w-full min-w-[300px] max-w-full cursor-pointer object-scale-down sm:block"
-              />
-            </div>
-          )}
+    <div className="relative flex justify-center overflow-hidden rounded-lg">
+      {postPhotos.length > 1 && (
+        <MdOutlineNavigateBefore
+          onClick={previous}
+          className="absolute left-0 top-[50%]  h-12 w-12 cursor-pointer"
         />
-        {postPhotos.length > 1 && (
-          <MdOutlineNavigateNext
-            onClick={next}
-            className="absolute right-0 top-[50%] h-12 w-12 cursor-pointer"
-          />
-        )}
+      )}
+      <div className="h-[500px] max-h-[500px] ">
+        <img
+          src={src}
+          className=" h-full w-full min-w-[300px] max-w-full cursor-pointer object-scale-down "
+        />
       </div>
-
-      <Modal.Content
-        name={`post_id_${id}`}
-        render={() => <ClickOnImagePreview src={src} />}
-      />
-    </Modal>
+      {postPhotos.length > 1 && (
+        <MdOutlineNavigateNext
+          onClick={next}
+          className="absolute right-0 top-[50%] h-12 w-12 cursor-pointer"
+        />
+      )}
+    </div>
   );
 }
